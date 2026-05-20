274fb64123f6fab50899aead106783b458e72df2
SimurazxTR <tiktoktr894@gmail.com>
2026-05-20 16:12:18 +0700

    cara instalasi

---
+++ CaraInstalasi.bash
@@ -0,0 +1,10 @@
+# 1. Install dependencies
+pip install python-telegram-bot==20.7 aiohttp asyncio
+
+# 2. Ganti BOT_TOKEN dan ADMIN_IDS
+# Edit file, cari bagian:
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+ADMIN_IDS = [123456789]
+
+# 3. Jalankan bot
+python telegram_ddos_bot_premium.py
